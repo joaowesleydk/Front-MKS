b6dc22aa64a5c5efd6f87b61f7bc19318ce76c35
João Wesley D. Kind <joaodkind@gmail.com>
2025-10-21 16:20:11 -0300

    correção do product

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import ProductCarousel from "./ProductCarousel";
+import ProductCarousel from "./ProductCarousel.jsx";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay, Navigation, Pagination } from "swiper/modules";
 import "swiper/css";
