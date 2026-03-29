ccb9cbcb5d387540d98e6206b10df06d9447f682
João Wesley D. Kind <joaodkind@gmail.com>
2026-03-29 08:28:45 -0300

    Update carouselService.js

--- src/services/carouselService.js
+++ src/services/carouselService.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const API_URL = import.meta.env.VITE_API_URL;
 
 export const carouselService = {
-  getAll: () => axios.get(`${API_URL}/api/carousel`),
+  getAll: () => axios.get(`${API_URL}/api/products/carousel`),
   create: (data) => axios.post(`${API_URL}/api/carousel`, data),
   update: (id, data) => axios.put(`${API_URL}/api/carousel/${id}`, data),
   delete: (id) => axios.delete(`${API_URL}/api/carousel/${id}`)
